8fc783bad15ba535ad0fb42f94274381de0e6cd1
성재영 <jaeyeong@jaeyeong-MacBookPro.local>
2022-01-27 22:35:08 +0900

    Completed 5 basic

---
+++ inflearn/basic/5.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+  // 내 풀이
+  // return arr.sort((a, b) => a - b)[0];
+
+  // 풀이
+  let answer;
+  let min = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+  }
+  answer = min;
+  return answer;
+}
+
+let arr = [5, 7, 3, 2, 9, 11];
+console.log(solution(arr));

338037104ebc9323dc40aaaa1f99cf950bc64442
sungjaeyeong <wodud2587@gmail.com>
2022-01-31 19:48:49 +0900

    Completed 1 array

--- inflearn/array/1.js
+++ inflearn/array/1.js
@@ -1,4 +1,12 @@
-function solution(arr) {}
+function solution(arr) {
+  // me
+  let answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
+  }
+  return answer;
+}
 
 let arr = [7, 3, 9, 5, 6, 12];
 console.log(solution(arr));

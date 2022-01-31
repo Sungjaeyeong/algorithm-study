bde29ff8b25dce7f9f3881c6844394ec6388fc18
sungjaeyeong <wodud2587@gmail.com>
2022-01-31 19:50:05 +0900

    Completed 7 array

--- inflearn/array/7.js
+++ inflearn/array/7.js
@@ -1,4 +1,24 @@
-function solution(arr) {}
+function solution(arr) {
+  let answer = 0;
+  let dx = [-1, 1, 0, 0];
+  let dy = [0, 0, -1, 1];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      for (let k = 0; k < 4; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        if (nx < 0 || nx >= arr.length || ny < 0 || ny >= arr.length) {
+        } else {
+          if (arr[nx][ny] >= arr[i][j]) break;
+        }
+        if (k === 3) {
+          answer++;
+        }
+      }
+    }
+  }
+  return answer;
+}
 
 let arr = [
   [5, 3, 7, 2, 3],

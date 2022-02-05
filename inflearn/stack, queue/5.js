a23491f5dcb02bfefffc4be88c059cb36e22b8f3
sungjaeyeong <wodud2587@gmail.com>
2022-02-05 19:47:20 +0900

    Completed 5 stack

---
+++ inflearn/stack, queue/5.js
@@ -0,0 +1,33 @@
+function solution(s) {
+  // me
+  // let answer = 0;
+  // let count = 0;
+  // for (let i = 0; i < s.length; i++) {
+  //   if (s[i] === '(') {
+  //     count++;
+  //   } else {
+  //     count--;
+  //     if (s[i - 1] === '(') {
+  //       answer += count;
+  //     } else {
+  //       answer++;
+  //     }
+  //   }
+  // }
+  // return answer;
+
+  let answer = 0;
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') stack.push('(');
+    else {
+      stack.pop();
+      if (s[i - 1] === '(') answer += stack.length;
+      else answer++;
+    }
+  }
+  return answer;
+}
+
+let a = '()(((()())(())()))(())';
+console.log(solution(a));

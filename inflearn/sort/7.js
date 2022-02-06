70c4286a35ac0044dac1d9496564280cb0b475a2
성재영 <jaeyeong@jaeyeong-MacBookPro.local>
2022-02-06 20:19:13 +0900

    Completed 7 sort

---
+++ inflearn/sort/7.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+  // me
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    } else {
+      return a[0] - b[0];
+    }
+  });
+  return arr;
+}
+
+let arr = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution(arr));

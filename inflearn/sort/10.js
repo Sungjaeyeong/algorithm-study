f806c2b0e60dceeacaed3296ab2dd1daad9f4e76
Sungjaeyeong <wodud2587@gmail.com>
2022-02-07 21:56:43 +0900

    Completed 10 sort

--- inflearn/sort/10.js
+++ inflearn/sort/10.js
@@ -1,18 +1,32 @@
 function solution(target, arr) {
   // me
+  // arr.sort((a, b) => a - b);
+  // let answer = -1;
+  // let a = 0;
+  // let b = arr.length;
+  // while (b - a > 0) {
+  //   let mid = Math.floor((a + b) / 2);
+  //   if (arr[mid] === target) return mid + 1;
+  //   if (arr[mid] > target) {
+  //     b = mid;
+  //   } else {
+  //     a = mid;
+  //   }
+  // }
+  // return answer;
+  let answer;
   arr.sort((a, b) => a - b);
-  let answer = -1;
-  let a = 0;
-  let b = arr.length;
-  while (b - a > 0) {
-    let mid = Math.floor((a + b) / 2);
-    if (arr[mid] === target) return mid + 1;
-    if (arr[mid] > target) {
-      b = mid;
-    } else {
-      a = mid;
-    }
+  let lt = 0,
+    rt = arr.length - 1;
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) rt = mid - 1;
+    else lt = mid + 1;
   }
+
   return answer;
 }
 
